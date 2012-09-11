4e3768f06ab010a9e31591412b8a11adec22b78c
Lucas Riutzel <lriutzel@gmail.com>
2012-09-10 23:14:17 -0500

    update install.sh to work with the new HOME dir

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+# allow the installation destionation to be passed in as arg1
+# else default to the current user's HOME
 if (($+1)); then
   DESTINATION=$1
 else
   DESTINATION=$HOME
 fi
 
-for file in $(find $DIR -maxdepth 1); do
- file=${file#$DIR/}
+for file in $(find $DIR/HOME -maxdepth 1); do
+ file=${file#$DIR/HOME/}
  
- if [[ $file =~ '^[a-z]' ]] && [[ $file != 'install.sh' ]]
+ if [[ $file =~ '^[a-z]' ]]
  then
-   echo "ln -s \e[1;34m$DIR\e[00m/$file \e[0;35m$DESTINATION\e[00m/.$file"
+   echo "ln -s \e[1;34m$DIR/HOME\e[00m/$file \e[0;35m$DESTINATION\e[00m/.$file"
  fi
 done
 echo ""
@@ -24,35 +26,38 @@ if [[ $REPLY != "y" ]]; then
   exit 1
 fi
 
-for file in $(find $DIR -maxdepth 1); do
- file=${file#$DIR/}
+for file in $(find $DIR/HOME -maxdepth 1); do
+ file=${file#$DIR/HOME/}
  
- if [[ $file =~ '^[a-z]' ]] && [[ $file != 'install.sh' ]]
+ if [[ $file =~ '^[a-z]' ]]
  then
    
    # does file already exist?
-   if [ -e "$DESTINATION/.$file" ]
+   if [ -e "$DESTINATION/.$file" ] || [ -h "$DESTINATION/.$file" ]
    then
     echo -n ".$file exists. Overwrite? (\e[1;32my\e[00m/\e[00;31mn\e[00m) "
     read OVERWRITE
     if [[ $OVERWRITE == 'y' ]]
     then
-      if [[ -f "$DESTINATION/.$file" ]]
+      if [[ -f "$DESTINATION/.$file" ]] || [[ -h "$DESTINATION/.$file" ]] 
       then
         rm "$DESTINATION/.$file"
       fi
 
       if [[ -d "$DESTINATION/.$file" ]]
       then
-        rm -r "$DESTINATION/.$file"
+        rm -r "$DESTINATION/.$file":
       fi
+      echo "\e[1;32mLinking $file\e[00m"
+      ln -s $DIR/HOME/$file $DESTINATION/.$file
     else
       echo "\e[1;31mSkipping file\e[00m"
       continue
     fi
+   else
+     echo "\e[1;32mLinking $file\e[00m"
+     ln -s $DIR/HOME/$file $DESTINATION/.$file
    fi
    
-   echo "\e[1;32mLinking $file\e[00m"
-   ln -s $DIR/$file $DESTINATION/.$file
  fi
 done
