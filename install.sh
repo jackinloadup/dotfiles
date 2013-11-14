8766a966b632e52eaeb4fc518698c9c259496bea
Lucas Riutzel <lriutzel@gmail.com>
2013-11-14 11:29:36 -0600

    symlink .dotfiles in the destination directory

--- install.sh
+++ install.sh
@@ -1,12 +1,48 @@
 #!/bin/zsh
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+DIR="$(readlink -f "$( cd "$( dirname "$0" )" && pwd )")"
 
-# allow the installation destionation to be passed in as arg1
+# allow the installation destination to be passed in as arg1
 # else default to the current user's HOME
 if (($+1)); then
   DESTINATION=$1
 else
   DESTINATION=$HOME
+fi
+
+# get the absolute location of our destination
+DESTINATION="$(readlink -f "$DESTINATION")"
+
+# check if we are running the current script out of our desired location
+if [ `readlink -f $DESTINATION/.dotfiles` != $DIR ]; then
+  #debug
+  #echo $DIR
+  #echo `readlink -f $DESTINATION/.dotfiles`
+  if [ -h $DESTINATION/.dotfiles ]; then # check if desired location is symlink
+    # at this point we know the desired destination is a symlink but not the one we want
+    echo "There seems to be a symlinked directory where i would like to put mine."
+    echo "I would like to symlink $DESTINATION/.dotfiles to $DIR"
+    echo "Currently it is symlinked to `readlink -f $DESTINATION/.dotfiles`"
+    echo "I'm going to let you sort that out."
+    echo "Abandon ship!"
+    exit 1;
+  else # desired location is not symlink
+    if [ -e $DESTINATION/.dotfiles ]; then # check if file exists in the desired location
+      echo "file exists"
+    else # file does not exist in the desired location
+      echo "I see you are using an alternate dotfiles directory location. I need to create a link to myself that I know of"
+      echo "May I symlink $DIR to $DESTINATION "
+      echo -n "Create? (\e[1;32my\e[00m/\e[00;31mn\e[00m) "
+      read CREATE
+      if [[ $CREATE == "y" ]]; then
+        ln -s $DIR $DESTINATION/.dotfiles
+      else
+        echo "Sounds good boss. I'm going to let you sort that out then."
+        echo "Abandon ship!"
+        exit 1;
+      fi
+    fi
+  fi
+  echo "woot"
 fi
 
 for file in $(find $DIR/HOME -maxdepth 1); do
